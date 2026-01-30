82d7b4ea5de73aee51f64be54fd0acb6df29757f
Sanheeth Singh <sanheethsingh@gmail.com>
2026-01-30 20:31:54 +0530

    Update api.py

--- Flask_API/api.py
+++ Flask_API/api.py
@@ -15,9 +15,10 @@ api.add_resource(User, '/api/users/<int:id>')
 
 @app.route("/")
 def home():
-    return render_template('project_info.html')
+    return <h1>User Management RESTful API</h1>
 
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
+
     app.run(debug=True)
